757f25f5759777d25ad9a41221bf924b24eff78c
Moonstar <romerotaisha123@gmail.com>
2026-05-05 12:49:58 -0400

    Update attack.py

--- Practice-Problem-3/attack.py
+++ Practice-Problem-3/attack.py
@@ -1,3 +1,6 @@
+# Defines the Archer class as a child of Character, adding an arrows attribute,
+# overriding the attack method with a ranged attack, and extending __str__ to show arrows.
+
 #import parent class from problem 1+ child classes from problem 2
 from character import character
 from warrior import warrior,mage, guild
