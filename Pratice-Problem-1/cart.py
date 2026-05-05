9ff24aa9069478a03a7c3eb5b22aa7b67112fd07
Moonstar <romerotaisha123@gmail.com>
2026-05-05 12:13:55 -0400

    Delete Pratice-Problem-1 directory

--- Pratice-Problem-1/cart.py
+++
@@ -1,9 +0,0 @@
-def cartTotal(prices):
-    prices = [1.99,2.99,3.99]
-    total = prices[0] + prices[1] + prices[2]
-    total += total * 0.06
-    taxTotal = total
-    return taxTotal
-prices = [1.99,2.99,3.99]
-
-print(cartTotal(prices))
