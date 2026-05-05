d5349e97885544d24d981b90d9c85257c64f78cf
Moonstar <romerotaisha123@gmail.com>
2026-05-05 12:15:04 -0400

    Add files via upload

---
+++ Practice-Problem-1/cart.py
@@ -0,0 +1,9 @@
+def cartTotal(prices):
+    prices = [1.99,2.99,3.99]
+    total = prices[0] + prices[1] + prices[2]
+    total += total * 0.06
+    taxTotal = total
+    return taxTotal
+prices = [1.99,2.99,3.99]
+
+print(cartTotal(prices))
