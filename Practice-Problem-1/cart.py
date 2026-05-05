431a9310b235d5bf15fefcef9255232e7bc784cb
Moonstar <romerotaisha123@gmail.com>
2026-05-05 12:35:25 -0400

    Update cart.py

--- Practice-Problem-1/cart.py
+++ Practice-Problem-1/cart.py
@@ -1,3 +1,4 @@
+#This function takes a list of item prices, adds them together, applies a 6% sales tax, and returns the final total.
 def cartTotal(prices):
     prices = [1.99,2.99,3.99]
     total = prices[0] + prices[1] + prices[2]
